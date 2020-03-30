22dd63a31d85a37047bda5ebeb481306442f21a0
nerrons <coniinedieu@gmail.com>
2020-03-30 17:58:38 +0800

    Set up xcodeproj; clean up project structure

---
+++ WaterSupport/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  effervescence-app
+//
+//  Created by Nerrons on 29/3/20.
+//  Copyright © 2020 nerrons. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
